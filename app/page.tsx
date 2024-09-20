3a3cc681ff0f6de221a6bccf050edb4bbb2d81a9
tristanshaw000 <tristanshaw000@gmail.com>
2024-09-20 14:31:48 +0000

    fullscreen mode

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ function Home() {
   const [url, setUrl] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   let finalUrl: string;
 
@@ -19,9 +20,12 @@ function Home() {
     if (url) {
       setIsLoading(true);
       setError(null);
+      setIsLoaded(false);
 
-      if (!url.startsWith('https://')) finalUrl = `https://${url.toLowerCase()}`;
-      else finalUrl = url;
+      const lowerCaseUrl = url.toLowerCase();
+
+      if (!lowerCaseUrl.startsWith('http://') && !lowerCaseUrl.startsWith('https://')) finalUrl = `https://${lowerCaseUrl}`;
+      else finalUrl = lowerCaseUrl;
 
       try {
         const result = await fetchWebsite(finalUrl);
@@ -33,39 +37,48 @@ function Home() {
             doc.close();
           }
         }
-      } catch (error) { setError(error instanceof Error ? error.message : 'An error occurred');
-      } finally { setIsLoading(false);
+        setIsLoaded(true);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : 'An error occurred');
+      } finally {
+        setIsLoading(false);
       }
     }
   };
 
   return (
-    <div className="min-h-screen bg-background text-foreground flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-6">Custos</h1>
-      <h3 className="text-2xl font-bold mb-6">Privacy first</h3>
-      <form onSubmit={handleSearch} className="flex mb-4 w-full max-w-md">
-        <input
-          type="text"
-          className="flex-grow p-2 rounded-l-md bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Enter website URL"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="
-            p-2 bg-blue-600 rounded-r-md 
-            hover:bg-blue-500 transition duration-200"
+    <div className="overflow-hidden min-h-screen bg-background text-foreground flex flex-col items-center justify-center p-0">
+      {!isLoaded && (
+        <>
+          <h1 className="text-4xl font-bold mb-6">Custos</h1>
+          <h3 className="text-2xl font-bold mb-6">Privacy first</h3>
+        </>
+      )}
+      {!isLoaded && (
+        <form onSubmit={handleSearch} className="flex mb-4 w-full max-w-md">
+          <input
+            type="text"
+            className="flex-grow p-2 rounded-l-md bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter website URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="
+              p-2 bg-blue-600 rounded-r-md 
+              hover:bg-blue-500 transition duration-200"
             disabled={isLoading}>
-          Search
-        </button>
-      </form>
+            Search
+          </button>
+        </form>
+      )}
       {isLoading && <p className="text-gray-400">Loading and building site...</p>}
       {error && <p className="text-red-500">Error building website: {error}</p>}
-      <div className="mt-4 w-full max-w-2xl">
+      <div className="mt-4 w-full h-full">
         <iframe
           ref={iframeRef}
-          className="w-full h-96 outline-none border-none rounded mt-2"
+          className="w-full h-screen outline-none border-none"
         />
       </div>
     </div>
